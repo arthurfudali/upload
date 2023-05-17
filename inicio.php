e7c781a0829549d41c69001f00a7fdcaa0e1c73c
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2023-05-17 15:06:35 -0300

    comentarios no codigo

--- inicio.php
+++ inicio.php
@@ -8,10 +8,10 @@
 </head>
 <body>
 
-<form name="upload" enctype="multipart/form-data" method="post" action="upload.php">
-        <input type="hidden" name="MAX_FILE_SIZE" value="99999999">
-        <input type="file" name="arquivo[]" multiple="multiple" />
-        <input name="enviar" type="submit" value="Enviar">
+<form name="upload" enctype="multipart/form-data" method="post" action="upload.php"> <!-- enctype e extremamente necessario nesse codigo, sem isso ele nao funciona -->
+        <input type="hidden" name="MAX_FILE_SIZE" value="99999999">  <!-- tamanho maximo de arquivo suportado -->
+        <input type="file" name="arquivo[]" multiple="multiple" /> <!-- multiple="multiple" permite selecionar varios arquivos --> <!-- o [] no 'arquivo[]' serve para dar codigos a cada arquivo individualmente -->
+        <input name="enviar" type="submit" value="Enviar"> 
 </form>
     
 </body>
--- upload.php
+++ upload.php
@@ -1,12 +1,13 @@
 <?php
 
-$diretorio = "arquivos/";
+$diretorio = "arquivos/"; /* onde vai cair os arquivos */ /* barra 1 */
 
-$arquivo = isset($_FILES['arquivo']) ? $_FILES['arquivo'] : FALSE;
+$arquivo = isset($_FILES['arquivo']) ? $_FILES['arquivo'] : FALSE; /* operador TERNARIO UUUUUUUUUUUUUUUUUUUUUUUUUUUU */ /* isset -> serve pra ver se a variavel existe */ 
 
-for ($k = 0; $k < count($arquivo['name']); $k++){
+for ($k = 0; $k < count($arquivo['name']); $k++){ /* o count conta quantos arquivos existem, com isso ele conta a quatidade com k<quantidade */
 	
-	$destino = $diretorio."/".$arquivo['name'][$k];
+	$destino = $diretorio."/".$arquivo['name'][$k]; /* para onde o arquivo vai */   /* barra 2 */
+	/* arquivo//copo.png -> ele usa duas barras  */
 
 	if (move_uploaded_file($arquivo['tmp_name'][$k], $destino)){
 		echo "Arquivo enviado com sucesso!";
