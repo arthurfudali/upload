cff34699936da6a37b37182297d8c013f64d399d
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2023-05-17 15:25:47 -0300

    atividade (nao funcional)

--- upload.php
+++ upload.php
@@ -1,8 +1,14 @@
 <?php
 
-$diretorio = "arquivos/"; /* onde vai cair os arquivos */ /* barra 1 */
+// atividade: 1. subir apenas arquivos.png 2. dar nomes aleatorios a todos os arquivos 
 
+
+//pathinfo --> PATHINFO_EXTENSION
+//md5() nao funciona sozinho pois uma palavra sempre tera a mesma cripto
+//uniqid() 
+$diretorio = "arquivos/"; /* onde vai cair os arquivos */ /* barra 1 */
 $arquivo = isset($_FILES['arquivo']) ? $_FILES['arquivo'] : FALSE; /* operador TERNARIO UUUUUUUUUUUUUUUUUUUUUUUUUUUU */ /* isset -> serve pra ver se a variavel existe */ 
+if(pathinfo(['arquivo'],PATHINFO_EXTENSION) == '.png'){
 
 for ($k = 0; $k < count($arquivo['name']); $k++){ /* o count conta quantos arquivos existem, com isso ele conta a quatidade com k<quantidade */
 	
@@ -15,5 +21,8 @@ for ($k = 0; $k < count($arquivo['name']); $k++){ /* o count conta quantos arqui
 		echo "Falha ao enviar o arquivo";
 	}
 }
-
+}
+else{
+	echo 'aceitamos apenas arquivos .png';
+}
 ?>
