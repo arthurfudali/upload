8578236a0b1a250c69471a56a2ce2a565a324d28
Arthur Ribeiro Dias Fudali <99662198+arthurfudali@users.noreply.github.com>
2023-06-20 23:02:20 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -28,7 +28,7 @@ for ($k = 0; $k < count($arquivo['name']); $k++) {
 	/* move_uploaded_file(arquivo, destino do arquivo) */
 	if ($extension == "png") {
 		/* Mover arquivo para a pasta com o nome randomico */
-		if (move_uploaded_file($arquivo['tmp_name'][$k], $diretorio . "/" . $varImgNameRandom . "." . $extension)) {
+		if (move_uploaded_file($arquivo['tmp_name'][$k], $diretorio . "/" . $nomeImagemRandom . "." . $extension)) {
 
 			mysqli_query($conexao, "INSERT INTO imagens (nome_imagem, nome_randomico_imagem) VALUES ('$nomeImagem', '$nomeImagemRandom.$extension')");
 			echo " <br> Arquivo enviado com sucesso! <br>";
